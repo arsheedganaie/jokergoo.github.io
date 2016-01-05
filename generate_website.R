79b94ee30a70f4acd2a9512bb767d71bc65beb6b
Zuguang Gu <jokergoo@gmail.com>
2016-01-05 12:10:36 +0100

    Update generate_website.R

--- generate_website.R
+++ generate_website.R
@@ -32,18 +32,11 @@ footer = "
 ## index.html
 
 html = c(header,
-"<p>Hi there. My name is Zuguang Gu. Currently I am working at German Cancer Research Center (DKFZ) as a postdoc. 
-My major interest includes NGS data analysis and software development.
-</p>
-<p style='text-align:center'>
-<script type=\"text/javascript\">
-          Math.random() < 0.5 ? document.write(\"<img style='width:500px' src='image/image1.jpg' />\") 
-                              : document.write(\"<img style='width:500px' src='image/image2.jpg' />\") ;
-</script>
-</p>
-", footer)
+markdownToHTML("index.md", useBytes = TRUE),
+footer)
 
 writeLines(html, "index.html", useBytes = TRUE)
+
 
 ## software.html
 html = c(header,
