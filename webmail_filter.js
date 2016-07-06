032ee8dd083a7cfa4dfa7e23e52956bfbbacb2b4
Zuguang Gu <jokergoo@gmail.com>
2016-07-06 12:56:31 +0200

    Update webmail_filter.js

--- webmail_filter.js
+++ webmail_filter.js
@@ -6,9 +6,15 @@ var subject = new Array();
 var rows = new Array();
 
 load_all = function() {
-	rows = document.getElementsByClassName("lvm");
-	alert(rows[0].innerHTML);
-	rows = document.getElementsByClassName("lvm")[0].lastChild.childNodes;
+	var all_table = document.getElementsByTagName("table");
+	for(var i = 0; i < all_table.length; i ++) {
+		if(all_table[i].className === "lvw") {
+			table = all_table[i];
+			break;
+		}
+	}
+
+	rows = table.lastChild.childNodes;
 
 	unread.length = rows.length;
 	checked.length = rows.length;
@@ -20,24 +26,30 @@ load_all = function() {
 		var td = rows[i].childNodes;
 		unread[i] = td[1].firstChild.getAttribute("alt") == "Message: Unread";
 		checked[i] = td[3].firstChild;
-		from[i] = td[4].innerHTML;
-		subject[i] = td[5].innerHTML;
+		from[i] = td[4].innerHTML.replace(/&nbsp;/g, "");
+		subject[i] = td[5].getElementsByTagName("a")[0].innerHTML.replace(/&nbsp;/g, "");
 	}
 }
 
 popup_frame = function() {
 
-	load_all()
+	load_all();
 
 	// create a div
-	var frame = document.createElement("div")
+	var frame = document.createElement("div");
 	frame.innerHTML = 
 "<ul>\n" +
 "<li><input type='checkbox' value='1' onchange='execute_query(this.value, \"unread\")'>unread</li>\n" +
 "<li>From:<input type='text' value='' onKeypress='execute_query(this.value), \"from\"'></li>\n" +
 "<li>subject:<input type='text' value='' onkeypress='execute_query(this.valule, \"subject\")'></li>\n" +
 "</ul>\n";
-	frame.style = "position:absolute;"
+	frame.style.position = "fixed";
+	frame.style.border = "1px solid grey";
+	frame.style.left = "0px";
+	frame.style.top = "0px";
+	frame.style.padding = "4px";
+	frame.style.zIndex = "100";
+	document.body.appendChild(frame);
 }
 
 var unread_value = 0;
@@ -55,16 +67,16 @@ execute_query = function(value, name) {
 		subject_value = value;
 	}
 
-	if(!unread_value && from_value === "" && subject_value === "") {
-		return(null)
-	}
-
-	for(var i = 0; i < rows.length(); i ++) {
-		checked[i].unchecked();
+	for(var i = 2; i < rows.length; i ++) {
+		checked[i].setAttribute("checked", 0);
 
 		select = false;
-		if(unread_value && unread[i]) {
-			select = select && true;
+		if(unread_value) {
+			if(unread[i]) {
+				select = select && true;
+			} else if() {
+				select = false;
+			}
 		}
 
 		if(from_value !== "" && from[i].match(from_value)) {
@@ -81,4 +93,4 @@ execute_query = function(value, name) {
 	}
 }
 
-popup_frame()
+popup_frame();
